dce8771e99814e8392d585fc06f602ee0cbc5e2d
Christoph Fischer <chris@toph.de>
2014-11-27 20:23:42 +0100

    Added submenu.php for menu entry

---
+++ submenu.php
@@ -0,0 +1,7 @@
+<?php
+$my_submenu['rota']['rota']['vmfds_progmatic2014_home'] = array(
+    'output' => getLL('vmfds_progmatic2014_menu'),
+    'link' => $ko_path.'rota/index.php?action=vmfds_progmatic2014_home',
+    'html' => '',
+    'show' => '',
+);
